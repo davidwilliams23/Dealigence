063333a39cc0d1cd4fc963715c4fd67c1793117a
David <davidwilliams23@comcast.net>
2012-12-24 06:48:47 -0800

    Working App

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def login
+  end
+
+  def contact_us
+  end
+
+  def about
+  end
+
+  def categories
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dealigence::Application.config.secret_token = '3017ad7f4866ba15520091c64bfc6c17e170c07e4dff8b3dba2a3b64c477c2924e194c3dfc894142772ebc1db07aba96f6a589bd9c8d9b94e7381b71a6326a04'
